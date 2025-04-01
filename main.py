954056d21f78e12c68b8c3eb779578b973035411
Final Grade: Hacked <john.doe010424@protonmail.com>
2025-04-01 17:12:37 +0200

    new code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Vor dem abgeben die "Debug:" sachen entfernen
 # Import's:
 from colorama import Fore, Style
 import sys
@@ -7,7 +8,7 @@ import secrets
 # Generate Password
 passwd = secrets.token_urlsafe(5)
 
-def slowprint(s): # Slowprint von https://gist.github.com/gnuton/3c7a46447d2be0aee0b2
+def slowprint(s): # Slowprint by https://gist.github.com/gnuton/3c7a46447d2be0aee0b2
     for c in s + '\n':
         sys.stdout.write(c)
         sys.stdout.flush()
@@ -17,11 +18,11 @@ def slowprint(s): # Slowprint von https://gist.github.com/gnuton/3c7a46447d2be0a
 
 # Funktionen
 def anleitung():
-    print("Julius, schreib hier mal die Anleitung rein!!!")
+    print("Debug: Julius, schreib hier mal die Anleitung rein!!!")
     while True:
         start = input("Willst du starten? (j/n): ").lower()
         if start == "j":
-            print("Lets go")
+            print("Debug: Start")
             first()
             break
         elif start == "n":
@@ -32,18 +33,24 @@ def anleitung():
 
 def first():
     print("Du bist in das Zimmer des Schulleiters eingebrochen und hast den Laptop gefunden. Was willst du nun tun?")
-    print(Fore.BLUE + "Den Laptop starten?" + Style.RESET_ALL)
-    print(Fore.BLUE + "Die Festplatte kopieren?" + Style.RESET_ALL)
-    print(Fore.BLUE + "Die Festplatte stehlen?" + Style.RESET_ALL)
-    while True:
-        first = input("Willst du starten? (j/n): ").lower()
-        if first == "j":
-            print("Lets go")
-            break
-        elif first == "n":
-            print("Bye 👋")
-            exit()
-        else:
-            print("Bitte nur 'j' oder 'n' eingeben!")
+    print(Fore.BLUE + "a) Den Laptop starten?" + Style.RESET_ALL)
+    print(Fore.BLUE + "b) Die Festplatte kopieren?" + Style.RESET_ALL)
+    print(Fore.BLUE + "c) Die Festplatte stehlen?" + Style.RESET_ALL)
+    def first_abfrage():
+        while True:
+            first = input("Was wählst du?: ").lower()
+            if first == "a":
+                print("Debug: Input: a")
+                print("Der Laptop startet und du siehst ein Passwortfeld. Das Passwort weißt du nicht.")
+                continue
+            elif first == "b":
+                print("Debug: Input: b")
+
+            elif first == "c":
+                print("Debug: Input: c")
+                break
+            else:
+                print("Bitte nur 'a', 'b' oder 'c' eingeben!")
+    first_abfrage()
 
 anleitung()
