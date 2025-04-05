99f46c771a1a11ba06407247e06978eefbf3020c
Final Grade: Hacked <john.doe010424@protonmail.com>
2025-04-05 17:57:05 +0200

    New useless code

--- main.py
+++ main.py
@@ -53,72 +53,74 @@ def a():
             print(Fore.BLUE + "Drücke h für das hilfe menu 🧑‍⚕️" + Style.RESET_ALL)
             first = input("Was wählst du?: ").lower()
             if first == "a":
-                print(Fore.RED + "Du hast kein Bock zu lernen und giebst nach 30min auf." + Style.RESET_ALL)
+                print(Fore.RED + "Du hast kein Bock zu lernen und giebst nach 30 minuten auf." + Style.RESET_ALL)
                 continue
             elif first == "b":
                 b()
-                break
             elif first == "c":
-                c = input("Bist du dir sicher das du Starten willst")
+                c = input("Bist du dir sicher das du Starten willst").lower()
                 if c == "j":
                     print("Du bereitest alles vor und startest dein Hacking program 💻")
                     slp(1)
                     print("Output:")
                     slowprint(Fore.GREEN + "Hack failed, security to high" + Style.RESET_ALL)
                     slp(2)
-                    print(Fore.RED + "Am abend bekommst du eine E-Mail in der steht das man den Hack auf dich zurrück verfolgen konnte und du einen Schulverweß bekommst 😭")
+                    print(Fore.RED + "Am abend bekommst du eine E-Mail in der steht das man den Hack auf dich zurrück verfolgen konnte und du einen Schulverweiß bekommst 😭")
                     slp(10)
                     clear()
                     print("Deine eltern regen sich auf warum du sowas machst und sie nehmen dir deinen PC weg und zwingen dich zum Lernen")
                     print("Du bleibts trozdem sitzen!\nGame Over 😭")
                     exit()
+                elif c == "n":
+                    clear()
+                    print("Game Over 😭")
+                    exit()
             elif first == "d":
                 print("Die Lehrer lassen sich leider nicht bestechen 💵")
                 slp(1)
                 print("Game Over 😭")
+                exit()
             elif first == "h":
                 help()
             else:
                 print("Bitte nur 'a', 'b', 'c' oder 'd' eingeben!")
     first_abfrage()
 
-def b():
-    print(Fore.RED + "Du steckst dein USB stick ein und erstellst ein eins zu ein kopie der Festplatte." + Style.RESET_ALL)
-    print(Fore.GREEN + "Du verlässt das Büro des Schulleiters wieder." + Style.RESET_ALL)
-    print(Fore.GREEN + "Du startest über deinen Privaten PC eine offline Bruteforce attacke" + Style.RESET_ALL)
-    print("Output deines Terminals:")
-    slowprint(Fore.LIGHTGREEN_EX + "No password match found, should I try again with a bigger database? (y/n):" + Style.RESET_ALL)
-    b = input("").lower()
-    if b == "y":
-        slowprint("Retrying with 40GB database!")
-        slp(2)
-        slowprint("Success: password is: " + "'" + passwd + "'")
-        slp(3)
-        final()
-    elif b == "n":
-        print("Debug: n")
-    else:
-        print("Bitte nur 'y' oder 'n' eingeben!")
+def b(): # Ganz b noch neu
+    print("Du gehst zu Büro deines Schulleiters und knackst das Schloss!")
+    print("In seinem Büro findest du:")
+    print("     Seinen Laptop der aber Verschlüsselt ist 🔒")
+    print("     Einen Schlüssel 🔑")
+    print("     Einen Tresor 🏦")
+    slp(15)
+    clear()
+    print("a) Den Schlüssel am Tresor probieren?\nb) Den Laptop kopieren?\nc) Den Laptop klauen?")
+    b = input(Fore.CYAN + "Was willst du tun?" + Style.RESET_ALL)
+    if b == "a":
+        clear()
+        print("Er passt aber darin sind nur Dokumente und Bargeld")
 
 def final():
     print("Du hast das Passwort heraus gefunden!")
-    print("Du logs dich ein und enderst deine Noten soc das du bestehst aber es nicht auffällt.")
-    print("Du bestehst das SChuljahr und dein Eltern freuen sich.")
+    print("Du logs dich ein und änderst deine Noten so das du bestehst aber es nicht auffällt.")
+    print("Du bestehst das Schuljahr und dein Eltern freuen sich.")
     print("Sie fragen sich aber auch wie du das geschaft hast. Du erzälst eine Lüge und sie glauben dir.")
-    print("Du bekommst den neuen Laptop und gehst zufrieden in die Sommerferien.")
+    print("Du bekommst einen neuen Laptop und gehst zufrieden in die Sommerferien.")
 
 def help():
     print("a) Was ist bruteforce\nb) Creditst")
     help = input("Was willst du?")
     if help == "a":
+        clear()
         print("Brute-Force bezeichnet eine Angriffsmethode in der IT-Sicherheit, bei der alle möglichen Kombinationen von Passwörtern oder Schlüsseln systematisch ausprobiert werden, um Zugang zu einem geschützten System zu erhalten. Diese Methode ist sehr rechenintensiv und zeitaufwendig, wird jedoch häufig dann eingesetzt, wenn keine Schwachstellen im System vorliegen.")
-    elif help == "b"
+        exit()
+    elif help == "b":
         credits()
     else:
         print("Bitte nur 'a' oder 'b' eingeben!")
 
 def credits():
     clear()
-    print("")
+    print("Game: Final Grade Hacked\nGit: https://github.com/johannSo/Final-Grade-Hacked\nAuthors: Jonathan Soppa, Julius Brückner\nTools: Zed, GitHub")
 
 anleitung()
