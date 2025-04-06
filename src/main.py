77ebb96775dea47cba5a9cdbea5562feb6999a55
Final Grade: Hacked <john.doe010424@protonmail.com>
2025-04-06 20:18:25 +0200

    Almost final version

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 # Vor dem abgeben die "Debug:" sachen entfernen
+# Nach jedem input immer clearen
 # Import's:
 from colorama import Fore, Style
 import sys
@@ -44,7 +45,6 @@ def anleitung():
 def a():
     def first_abfrage():
         while True:
-            print("\n")
             print("Du musst es schaffen zu bestehen, was machst du?")
             print(Fore.BLUE + "a) Lernen für die nächsten Tests 📚" + Style.RESET_ALL)
             print(Fore.BLUE + "b) In das Büro des Schulleiters einbrechen 🔑" + Style.RESET_ALL)
@@ -53,12 +53,13 @@ def a():
             print(Fore.BLUE + "Drücke h für das hilfe menu 🧑‍⚕️" + Style.RESET_ALL)
             first = input("Was wählst du?: ").lower()
             if first == "a":
+                clear()
                 print(Fore.RED + "Du hast kein Bock zu lernen und giebst nach 30 minuten auf." + Style.RESET_ALL)
                 continue
             elif first == "b":
                 b()
             elif first == "c":
-                c = input("Bist du dir sicher das du Starten willst").lower()
+                c = input("Bist du dir sicher das du Starten willst? (j/n): ").lower()
                 if c == "j":
                     print("Du bereitest alles vor und startest dein Hacking program 💻")
                     slp(1)
@@ -75,6 +76,9 @@ def a():
                     clear()
                     print("Game Over 😭")
                     exit()
+                else:
+                    print("Bitte nur 'j' oder 'n' eingeben!")
+                    continue
             elif first == "d":
                 print("Die Lehrer lassen sich leider nicht bestechen 💵")
                 slp(1)
@@ -86,26 +90,71 @@ def a():
                 print("Bitte nur 'a', 'b', 'c' oder 'd' eingeben!")
     first_abfrage()
 
-def b(): # Ganz b noch neu
-    print("Du gehst zu Büro deines Schulleiters und knackst das Schloss!")
+def b():
+    print(Fore.BLUE + "Du gehst zu Büro deines Schulleiters und knackst das Schloss!")
     print("In seinem Büro findest du:")
     print("     Seinen Laptop der aber Verschlüsselt ist 🔒")
     print("     Einen Schlüssel 🔑")
-    print("     Einen Tresor 🏦")
+    print("     Einen Tresor 🏦" + Style.RESET_ALL)
     slp(15)
     clear()
-    print("a) Den Schlüssel am Tresor probieren?\nb) Den Laptop kopieren?\nc) Den Laptop klauen?")
-    b = input(Fore.CYAN + "Was willst du tun?" + Style.RESET_ALL)
-    if b == "a":
+    while True:
+        print("a) Den Schlüssel am Tresor probieren?\nb) Den Laptop kopieren?\nc) Den Laptop klauen?")
+        b = input(Fore.CYAN + "Was willst du tun? " + Style.RESET_ALL)
         clear()
-        print("Er passt aber darin sind nur Dokumente und Bargeld")
+        if b == "a":
+            print("Er passt aber darin sind nur Dokumente und Bargeld")
+            continue
+        elif b == "b":
+            print("Du steckst deine SSD ein und startest den kopiervorgang.")
+            slp(2)
+            print("Zu hause angekommen versuchst du den Laptop zu knacken.")
+            slp(2)
+            print("Output deines Hacking Programms:")
+            slowprint(Fore.GREEN + "Bruteforce 10% done" + Style.RESET_ALL)
+            slp(1)
+            slowprint(Fore.GREEN + "Bruteforce 30% done" + Style.RESET_ALL)
+            slp(1)
+            slowprint(Fore.GREEN + "Bruteforce 60% done" + Style.RESET_ALL)
+            slp(1)
+            slowprint(Fore.GREEN + "Bruteforce 90% done" + Style.RESET_ALL)
+            slp(1)
+            slowprint(Fore.GREEN + "Bruteforce 100% done" + Style.RESET_ALL)
+            print("Du hast das Passwort gefunden!")
+            slp(2)
+            print("Das Passwort ist: " + Fore.GREEN + passwd + Style.RESET_ALL)
+            slp(2)
+            final()
+        elif b == "c":
+            print("Du nimmst den Laptop mit und versuchst ihn zu knacken.")
+            slp(2)
+            print("Der Lehrer chekt die überwchungsanlage und sieht das du den Laptop mitgenommen hast.")
+            slp(2)
+            print("Er ruft die Polizei und du wirst verhaftet.")
+            slp(2)
+            print("Du bekommst einen Schulverweiß und bleibst sitzen.")
+            slp(2)
+            print("Game Over 😭")
+            exit()
+
 
 def final():
-    print("Du hast das Passwort heraus gefunden!")
-    print("Du logs dich ein und änderst deine Noten so das du bestehst aber es nicht auffällt.")
-    print("Du bestehst das Schuljahr und dein Eltern freuen sich.")
-    print("Sie fragen sich aber auch wie du das geschaft hast. Du erzälst eine Lüge und sie glauben dir.")
-    print("Du bekommst einen neuen Laptop und gehst zufrieden in die Sommerferien.")
+    final = input("beste.schule: Gieb das Passwort ein: ")
+    if final == passwd:
+        print("Du hast das Passwort heraus gefunden!")
+        slp(2)
+        print("Du logs dich ein und änderst deine Noten so das du bestehst aber es nicht auffällt.")
+        slp(2)
+        print("Du bestehst das Schuljahr und dein Eltern freuen sich.")
+        slp(2)
+        print("Sie fragen sich aber auch wie du das geschaft hast. Du erzälst eine Lüge und sie glauben dir.")
+        slp(2)
+        print("Du bekommst einen neuen Laptop und gehst zufrieden in die Sommerferien.")
+    else:
+        print(Fore.RED + "Das Passwort ist falsch! 😭" + Style.RESET_ALL)
+        slp(1)
+        final()
+    exit()
 
 def help():
     print("a) Was ist bruteforce\nb) Creditst")
--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "https://api.duckduckgo.com/?q=hello&format=json"
-response = requests.get(url)
-data = response.json()
-
-print(data)
